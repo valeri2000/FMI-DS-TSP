7cab9b433011b968f4cd8d9500a7000a86bc93af
Valeri Stanchev <stan4ev6@gmail.com>
2021-01-06 14:02:07 +0200

    Fix run() parameters

--- ISolution.h
+++ ISolution.h
@@ -1,14 +1,13 @@
 #ifndef __ISOLUTION_H_INCLUDED
 #define __ISOLUTION_H_INCLUDED
 
-#include "IGraph.h"
 #include <vector>
 #include <utility>
 
 class ISolution {
     public:
         virtual ~ISolution();
-        virtual std::pair<unsigned, std::vector<unsigned> > run(IGraph*) = 0; // result, path
+        virtual std::pair<unsigned, std::vector<unsigned> > run() = 0; // result, path
 };
 
 #endif
