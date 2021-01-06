41de74ff69186376bdb3dabc135769f5f325cad2
Valeri Stanchev <stan4ev6@gmail.com>
2021-01-06 11:49:39 +0200

    Implement Graph class

---
+++ Graph.cpp
@@ -0,0 +1,57 @@
+#include "Graph.h"
+#include <cassert>
+
+Graph::Graph() {
+    vertices = edges = 0;
+}
+
+Graph::Graph(std::ifstream& in) {
+    in >> vertices;
+    edges = 0;
+
+    while(in) {
+        unsigned u, v, w;
+        in >> u >> v >> w;
+        addEdge(u, v, w);
+    }
+}
+
+void Graph::saveToFile(std::ofstream& out) const {
+    out << vertices << '\n';
+
+    unsigned u, v, w;
+    unsigned pEdges = 0;
+    for(unsigned i = 0; i < vertices; ++i) {
+        u = i;
+        for(const std::pair<unsigned, unsigned>& p : data[i]) {
+            v = p.first;
+            w = p.second;
+
+            if(u < v) {
+                out << u << ' ' << v << ' ' << w << '\n';
+                pEdges++;
+            }
+        }
+    }
+
+    assert(pEdges == edges);
+}
+
+unsigned Graph::V() const {
+    return vertices;
+}
+
+unsigned Graph::E() const {
+    return edges;
+}
+
+const std::list<std::pair<unsigned, unsigned> >& Graph::adjacent(const unsigned& v) const {
+    return data[v];
+}
+
+void Graph::addEdge(const unsigned& u, const unsigned& v, const unsigned& w) {
+    data[u].push_back(std::make_pair(v, w));
+    data[v].push_back(std::make_pair(u, w));
+    edges++;
+}
+
---
+++ Graph.h
@@ -0,0 +1,26 @@
+#ifndef __GRAPH_H_INCLUDED
+#define __GRAPH_H_INCLUDED
+
+#include <vector>
+#include <list>
+#include <utility>
+#include <fstream>
+
+class Graph {
+    private:
+        unsigned vertices, edges;
+        std::vector<std::list<std::pair<unsigned, unsigned> > > data;
+
+    public:
+        Graph();
+        Graph(std::ifstream&);
+
+        void saveToFile(std::ofstream&) const;
+        unsigned V() const;
+        unsigned E() const;
+        const std::list<std::pair<unsigned, unsigned> >& adjacent(const unsigned&) const;
+
+        void addEdge(const unsigned&, const unsigned&, const unsigned&);
+};
+
+#endif
