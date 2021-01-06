459b717bff21dd196607b6071ddcd2bfd4f91d04
Valeri Stanchev <stan4ev6@gmail.com>
2021-01-06 12:35:47 +0200

    Fix GraphGenerator to create connected graph

--- GraphGenerator.cpp
+++ GraphGenerator.cpp
@@ -2,6 +2,7 @@
 #include <ctime>
 #include <cstdlib>
 #include <set>
+#include <algorithm>
 
 unsigned GraphGenerator::randBetween(const unsigned& left, const unsigned& right) {
     return left + rand() % (right - left) + 1;
@@ -11,11 +12,33 @@ Graph GraphGenerator::generate(const unsigned& vertices,
                                const unsigned& edges,
                                const unsigned& fromWeight,
                                const unsigned& toWeight) {
+    if(edges < vertices - 1 || vertices == 0) {
+        return Graph();
+    }
+    if(vertices == 1) {
+        return Graph(1);
+    }
+
     Graph g(vertices);
     srand(time(NULL));
-
     std::set<std::pair<unsigned, unsigned> > used;
-    unsigned edgesLeft = edges;
+
+    std::vector<unsigned> permutation;
+    for(unsigned i = 0 ; i < vertices; ++i) {
+        permutation.push_back(i);
+    }
+    std::random_shuffle(permutation.begin(), permutation.end());
+
+    for(unsigned i = 0; i < vertices - 1; ++i) {
+        unsigned u = permutation[i];
+        unsigned v = permutation[i + 1];
+
+        used.insert(std::make_pair(u, v));
+        unsigned w = randBetween(fromWeight, toWeight);
+        g.addEdge(u, v, w);
+    }
+
+    unsigned edgesLeft = edges - vertices + 1;
     while(edgesLeft) {
         unsigned u = randBetween(0, vertices - 2);
         unsigned v = randBetween(u + 1, vertices - 1);
